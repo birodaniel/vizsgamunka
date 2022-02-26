de80911c47549ccf0ada71c8f481324e3479ef50
Szabó Gergely <sz.gergely.sandor@gmail.com>
2022-02-26 14:08:32 +0100

    Login form added

--- src/views/index.php
+++ src/views/index.php
@@ -9,23 +9,29 @@
     <title><?= $title ?></title>
 </head>
 <body>
-<h1><?= $title ?></h1>
-<hr>
-<div>
-    <form>
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-        </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Jelszó</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
-        </div>
-        <button type="submit" class="btn btn-primary">Belépés</button>
-        <a href="./register/register.php" class="btn">Regisztráció</a>
-    </form>
-</div>
+<section class="m-5">
+    <div class="d-flex justify-content-center  h-100">
+        <form class="bg-info p-5">
+            <div class="mx-3 my-5  d-flex justify-content-center">
+                <h1><?= $title ?></h1>
+            </div>
+            <div class="mx-3 my-5">
+                <label for="exampleInputEmail1" class="form-label d-flex justify-content-center">Email</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="mx-3 my-5">
+                <label for="exampleInputPassword1" class="form-label d-flex justify-content-center">Jelszó</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="mx-3 my-5 d-flex justify-content-center">
+                <button type="submit" class="btn btn-success px-5">Belépés</button>
+            </div>
+            <div class="mx-3 my-5">
+                <a href="./register/register.php" class="btn btn-primary px-5  d-flex justify-content-center">Regisztráció</a>
+            </div>
+        </form>
+    </div>
+</section>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
