f054b395ca67617bb9fe067e193b32073fbd7536
Szabó Gergely <sz.gergely.sandor@gmail.com>
2022-02-26 23:43:36 +0100

    index.php and register.php updated

--- src/views/register/register.php
+++ src/views/register/register.php
@@ -11,14 +11,14 @@
     <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/style.css">
-    <title>Regisztráció</title>
+    <title><?= $title ?></title>
 </head>
 <body>
-<section>
-    <div class="d-flex justify-content-center">
-        <form class="grey-medium p-5  ">
-            <div class="mx-3 my-3  d-flex justify-content-center">
-                <h1 class="spartan">Regisztráció</h1>
+<section class="d-flex justify-content-center align-items-center h-100">
+    <div class="d-flex justify-content-center form-height">
+        <form class="grey-medium p-5">
+            <div class="mx-3 d-flex justify-content-center">
+                <h1 class="spartan"><?= $title ?></h1>
             </div>
             <div class="mx-3 my-2">
                 <label for="exampleInputPassword1" class="form-label d-flex justify-content-center grotesk">Név</label>
@@ -40,12 +40,12 @@
                 <label for="exampleInputPassword1" class="form-label d-flex justify-content-center grotesk">Jelszó újra</label>
                 <input type="password" class="form-control" id="exampleInputPassword1">
             </div>
-            <div class="mb-3 form-check">
+            <div class="mx-3 my-2 form-check">
                 <input type="checkbox" class="form-check-input" id="exampleCheck1">
                 <label class="form-check-label grotesk" for="exampleCheck1">Általános adatvédelmi nyilatkozat elfogadása</label>
             </div>
-            <div class="mx-3 my-2 d-flex justify-content-center">
-                <button type="submit" class="btn px-5 lime-button spartan">Registrál</button>
+            <div class="mx-2 my-2   d-flex justify-content-center">
+                <button type="submit" class="btn px-5 lime-button spartan">Regisztrál</button>
             </div>
         </form>
     </div>
