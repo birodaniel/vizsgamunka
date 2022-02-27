226e1cb1f47d9409a4f35e012f242d4e587947d7
Szabó Gergely <sz.gergely.sandor@gmail.com>
2022-02-27 17:05:29 +0100

    index.php updated

--- src/views/index.php
+++ src/views/index.php
@@ -14,28 +14,33 @@
     <title><?= $title ?></title>
 </head>
 <body>
-<section class="d-flex justify-content-center align-items-center h-100">
-    <div class="d-flex justify-content-center align-items-around form-height">
-        <form class="grey-medium p-5">
-            <div class="mx-4  d-flex justify-content-center">
-                <h1 class="spartan"><?= $title ?></h1>
-            </div>
-            <div class="mx-4 my-5">
-                <label for="exampleInputEmail1" class="form-label d-flex justify-content-center grotesk">Email</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            </div>
-            <div class="mx-4 my-5">
-                <label for="exampleInputPassword1" class="form-label d-flex justify-content-center grotesk">Jelszó</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
-            </div>
-            <div class="mx-4 my-5 d-flex justify-content-center">
-                <button type="submit" class="btn px-5 black-button spartan">Belépés</button>
-            </div>
-            <div class="mx-4 my-5 d-flex justify-content-center">
-                <a href="./register/register.php" class="btn lime-button spartan">Regisztráció</a>
-            </div>
-        </form>
+<nav class="navbar navbar-expand-lg navbar-light grey-medium d-flex justify-content-between">
+    <div class="container-fluid mx-5">
+        <h5 class="spartan"><?= $title ?></h5>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link spartan" href="#">Bejelentkezés</a>
+                </li>
+            </ul>
+        </div>
     </div>
+</nav>
+<section class="mx-5 mt-2 p-5 bg-light grey-white">
+    <h3 class="spartan">Ponttáblázat</h3>
+    <table class="table grotesk">
+        <tr>
+            <th scope="col">Név</th>
+            <th scope="col">Meccsszám</th>
+            <th scope="col">Győzelem</th>
+            <th scope="col">Döntetlen</th>
+            <th scope="col">Vereség</th>
+            <th scope="col">Pontszám</th>
+        </tr>
+    </table>
 </section>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
--- src/views/login/login.php
+++ src/views/login/login.php
@@ -11,14 +11,14 @@
     <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/style.css">
-    <title>Belépés</title>
+    <title><?= $title ?></title>
 </head>
 <body>
 <section class="d-flex justify-content-center align-items-center h-100">
     <div class="d-flex justify-content-center align-items-around form-height">
         <form class="grey-medium p-5">
             <div class="mx-4  d-flex justify-content-center">
-                <h1 class="spartan">Belépés</h1>
+                <h1 class="spartan"><?= $title ?></h1>
             </div>
             <div class="mx-4 my-5">
                 <label for="exampleInputEmail1" class="form-label d-flex justify-content-center grotesk">Email</label>
