8598cc10aa6338251d17f37ab85feef27d713cef
Szabó Gergely <sz.gergely.sandor@gmail.com>
2022-03-02 13:00:01 +0100

    join-tournament.php requested data from database

--- src/views/tournament/join-tournament.php
+++ src/views/tournament/join-tournament.php
@@ -8,7 +8,24 @@ $navbarItems = [
     '/' => 'Vissza a nyitólapra'
 ];
 include __DIR__ . '/../includes/navigation.php'; ?>
+<?php
+require_once './vizsgamunka/src/App/DB.php';
 
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+    $sql = 'SELECT lastname,
+                    firstname,
+                    jobtitle
+               FROM employees
+              ORDER BY lastname';
+
+    $q = $pdo->query($sql);
+    $q->setFetchMode(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    die("Could not connect to the database $dbname :" . $e->getMessage());
+}
+?>
 <section>
     <div class="d-flex justify-content-center">
         <form class="grey-medium p-5" method="post" action="/registration-success">
@@ -17,7 +34,15 @@ include __DIR__ . '/../includes/navigation.php'; ?>
             </div>
             <div class="mx-3 my-2">
                 <label for="name" class="form-label d-flex justify-content-center grotesk">Verseny neve</label>
-                <input type="text" class="form-control" id="name" name="name" required>
+                <select class="form-select" aria-label="Default select example">
+                    <option selected>Verseny választása</option>
+                    <?php if (!$sql) {?>
+                    <?php }  else {?>
+                    <?php while ($row = $q->fetch()): ?>
+                    <option value="<?php echo htmlspecialchars($row['game_name']) ?>"><?php echo htmlspecialchars($row['game_name']) ?></option>
+                    <?php endwhile; ?>
+                    <?php } ?>
+                </select>
             </div>
             <div class="mx-3 my-2">
                 <label for="username" class="form-label d-flex justify-content-center grotesk">Felhasználónév</label>
