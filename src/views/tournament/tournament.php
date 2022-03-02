55dfb86c2170273ff11ca4e1a8612ed5936dd5b4
Szabó Gergely <sz.gergely.sandor@gmail.com>
2022-03-02 12:15:40 +0100

    tournament.php requested data from database

--- src/views/tournament/tournament.php
+++ src/views/tournament/tournament.php
@@ -9,6 +9,25 @@ $navbarItems = [
 ];
 include __DIR__ . '/../includes/navigation.php'; ?>
 
+<?php
+require_once './vizsgamunka/src/App/DB.php';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+    $sql = 'SELECT lastname,
+                    firstname,
+                    jobtitle
+               FROM employees
+              ORDER BY lastname';
+
+    $q = $pdo->query($sql);
+    $q->setFetchMode(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    die("Could not connect to the database $dbname :" . $e->getMessage());
+}
+?>
+
 <section class="mx-5 mt-2 p-5 bg-light grey-white">
     <h3 class="spartan">Fordulók</h3>
     <table class="table grotesk">
@@ -19,6 +38,21 @@ include __DIR__ . '/../includes/navigation.php'; ?>
             <th scope="col">Vesztes neve</th>
             <th scope="col">Döntetlen</th>
         </tr>
+        <?php if (!$sql) {?>
+            <tr>
+                <td colspan="5">Nincsenek fordulók</td>
+            </tr>
+        <?php }  else {?>
+
+        <?php while ($row = $q->fetch()): ?>
+            <tr>
+                <td><?php echo htmlspecialchars($row['match_id']) ?></td>
+                <td><?php echo htmlspecialchars($row['turn_id']) ?></td>
+                <td><?php echo htmlspecialchars($row['result1']); ?></td>
+                <td><?php echo htmlspecialchars($row['result2']); ?></td>
+            </tr>
+        <?php endwhile; ?>
+        <?php } ?>
     </table>
     <h3 class="spartan">Ponttáblázat</h3>
     <table class="table grotesk">
@@ -29,6 +63,20 @@ include __DIR__ . '/../includes/navigation.php'; ?>
             <th scope="col">Vereség</th>
             <th scope="col">Pontszám</th>
         </tr>
+        <?php if (!$sql) {?>
+            <tr>
+                <td colspan="5">Nincsenek pontok</td>
+            </tr>
+        <?php }  else {?>
+
+            <?php while ($row = $q->fetch()): ?>
+                <tr>
+                    <td><?php echo htmlspecialchars($row['user_name']) ?></td>
+                    <td><?php echo htmlspecialchars($row['firstname']); ?></td>
+                    <td><?php echo htmlspecialchars($row['points']); ?></td>
+                </tr>
+            <?php endwhile; ?>
+        <?php } ?>
     </table>
     <h3 class="spartan">Játékosok</h3>
     <table class="table grotesk">
@@ -38,6 +86,21 @@ include __DIR__ . '/../includes/navigation.php'; ?>
             <th scope="col">Meccsszám</th>
             <th scope="col">Pontszám</th>
         </tr>
+        <?php if (!$sql) {?>
+            <tr>
+                <td colspan="5">Nincsenek játékosok</td>
+            </tr>
+        <?php }  else {?>
+
+            <?php while ($row = $q->fetch()): ?>
+                <tr>
+                    <td><?php echo htmlspecialchars($row['user_name']) ?></td>
+                    <td><?php echo htmlspecialchars($row['email']); ?></td>
+                    <td><?php echo htmlspecialchars($row['matched']); ?></td>
+                    <td><?php echo htmlspecialchars($row['points']); ?></td>
+                </tr>
+            <?php endwhile; ?>
+        <?php } ?>
     </table>
 </section>
 <?php include __DIR__ . '/../includes/script.php'; ?>
